121c09d347c5f72de1274456b1034d508dd889fb
Dioxide <31298281+mc0xyh@users.noreply.github.com>
2024-09-04 23:51:31 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,16 +3,16 @@
 INIL = True
 
 #账户管理
-EMAIL_ADDRESS = '202383690059@nuist.edu.cn'
-EMAIL_USERNAME = '202383690059@nuist.edu.cn'
-EMAIL_PASSWORD = 'z3oq75yvxE5pfWdj'
+EMAIL_ADDRESS = ''
+EMAIL_USERNAME = ''
+EMAIL_PASSWORD = ''
 
 #imap服务器
-IMAP_SERVER = 'imap.exmail.qq.com'
-IMAP_PORT = 993
-IMAP_SSL = True
+IMAP_SERVER = ''
+IMAP_PORT = 
+IMAP_SSL = 
 
 #smtp服务器
-SMTP_SERVER = 'smtp.exmail.qq.com'
-SMTP_PORT = 465
-SMTP_SSL = True
+SMTP_SERVER = ''
+SMTP_PORT = 
+SMTP_SSL = 
